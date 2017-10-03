105209eabfefcd6e9a569fd712b75fd223905ef7
Beatrice Bianca <bianca.luca96@gmail.com>
2017-10-03 21:12:39 +0300

    Changed database from oracle to mysql

--- src/main/resources/db/migration/V2017.07.15.15.30.00__create_customer_table.sql
+++ src/main/resources/db/migration/V2017.07.15.15.30.00__create_customer_table.sql
@@ -1,32 +1,11 @@
-DECLARE n_customer NUMBER(10);
+CREATE TABLE IF NOT EXISTS CUSTOMER (
+          ID INT(10) NOT NULL PRIMARY KEY auto_increment,
+          LAST_NAME VARCHAR(20) NOT NULL ,
+          FIRST_NAME VARCHAR(20) NOT NULL,
+          ADDRESS VARCHAR(100),
+          CNP INT(13) NOT NULL,
+          GENDER VARCHAR(20),
+          CUSTOMER_PHONE INT(20));
 
-BEGIN
-   SELECT COUNT(*) INTO n_customer FROM tab WHERE tname='CUSTOMER';
 
-   IF (n_customer = 0) THEN
-      EXECUTE IMMEDIATE
-      'CREATE TABLE CUSTOMER (
-          ID NUMBER(10) NOT NULL PRIMARY KEY,
-          LAST_NAME VARCHAR2(20) NOT NULL ,
-          FIRST_NAME VARCHAR2(20) NOT NULL,
-          ADDRESS VARCHAR2(100),
-          CNP NUMBER(13) NOT NULL,
-          GENDER VARCHAR2(20),
-          CUSTOMER_PHONE NUMBER(20) ) ';
-
-      EXECUTE IMMEDIATE
-      'CREATE SEQUENCE customer_id_incr START WITH 1';
-
-      EXECUTE IMMEDIATE
-      'CREATE OR REPLACE TRIGGER ID_CUSTOMER
-          BEFORE INSERT ON CUSTOMER
-          FOR EACH ROW
-
-          BEGIN
-            SELECT customer_id_incr.NEXTVAL
-            INTO   :new.id
-            FROM   dual;
-          END; ';
-   END IF;
-END;
 
--- src/main/resources/db/migration/V2017.07.15.15.45.00__create_room_table.sql
+++ src/main/resources/db/migration/V2017.07.15.15.45.00__create_room_table.sql
@@ -1,28 +1,4 @@
-DECLARE n_room NUMBER(10);
-
-BEGIN
-   SELECT COUNT(*) INTO n_room FROM tab WHERE tname='ROOM';
-
-   IF (n_room = 0) THEN
-      EXECUTE IMMEDIATE
-      'CREATE TABLE ROOM (
-          ID NUMBER(10) NOT NULL PRIMARY KEY,
-          SINGLE_BED NUMBER(10),
-          DOUBLE_BED NUMBER(10) )';
-
-      EXECUTE IMMEDIATE
-      'CREATE SEQUENCE room_id_incr START WITH 1';
-
-      EXECUTE IMMEDIATE
-      'CREATE OR REPLACE TRIGGER ID_ROOM
-          BEFORE INSERT ON ROOM
-          FOR EACH ROW
-
-          BEGIN
-            SELECT room_id_incr.NEXTVAL
-            INTO   :new.id
-            FROM   dual;
-          END; ';
-   END IF;
-END;
-
+CREATE TABLE IF NOT EXISTS ROOM (
+          ID INT(10) NOT NULL PRIMARY KEY auto_increment,
+          SINGLE_BED INT(10),
+          DOUBLE_BED INT(10) );
