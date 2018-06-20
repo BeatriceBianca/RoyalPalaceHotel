bc1e4bc29de7c5ec40965ff173c694824f70f11d
Beatrice Bianca <bianca.luca@mindit.ro>
2018-06-20 20:41:57 +0300

    Added ChartJs. Added chart for rooms per year and rooms per month.

--- src/main/resources/static/js/manager/manager.controller.js
+++ src/main/resources/static/js/manager/manager.controller.js
@@ -6,9 +6,9 @@
         .module('RoyalPalaceHotel')
         .controller('managerController', Controller);
 
-    Controller.$inject = ['$rootScope', '$scope', '$window', '$state', '$filter', 'ManagerService', 'MaidService'];
+    Controller.$inject = ['$rootScope', '$scope', '$window', '$state', '$filter', 'ManagerService', 'MaidService', 'ReservationsService'];
 
-    function Controller($rootScope, $scope, $window, $state, $filter, ManagerService, MaidService) {
+    function Controller($rootScope, $scope, $window, $state, $filter, ManagerService, MaidService, ReservationsService) {
         var _self = this;
 
         _self.username = "";
@@ -18,6 +18,8 @@
         _self.newOffer = null;
 
         $rootScope.user = null;
+
+        _self.loading = false;
 
         _self.logout = function () {
             $window.location.href = "/logout";
@@ -83,6 +85,88 @@
             else return true;
         };
 
+        _self.getReports = function () {
+            _self.changeChart('rooms', 0);
+        };
+
+        _self.changeChart = function(type, month) {
+
+            _self.loading = true;
+
+            $('#myChart').remove();
+            $('#addChart').append('<canvas id="myChart"></canvas>');
+
+            if (!type) {
+                type = _self.chartOption;
+                month = _self.monthOption;
+            }
+
+            ReservationsService
+                .getAllReservations()
+                .then(function (reservations) {
+
+                    var ctx = document.getElementById("myChart");
+
+                    if (type === 'rooms') {
+                        var roomsType = {
+                            Single: 0,
+                            Double: 0,
+                            Triple: 0,
+                            Twin: 0,
+                            Apartment: 0
+                        };
+
+                        var tot = 0;
+                        reservations.data.forEach(function (reservation) {
+
+                            if ((month !== "0" && ((new Date(reservation.arrivalDate)).getMonth() + 1).toString() === month) || month === 0) {
+                                reservation.rooms.forEach(function (room) {
+                                    var roomName = room.roomType.roomName;
+
+                                    if (roomName.includes('Apartment')) {
+                                        roomsType.Apartment++;
+                                        tot++;
+                                    } else {
+                                        roomsType[roomName]++;
+                                        tot++;
+                                    }
+                                });
+                            }
+                        });
+
+                        if (tot) {
+                            var data = {
+                                datasets: [{
+                                    backgroundColor: ['#7165b2', '#8c9de2', '#bea4d2', '#bdccff', '#f0e2f8'],
+                                    data: [roomsType.Single, roomsType.Double, roomsType.Triple, roomsType.Twin, roomsType.Apartment]
+                                }],
+
+                                // These labels appear in the legend and in the tooltips when hovering different arcs
+                                labels: [
+                                    'Single',
+                                    'Double',
+                                    'Triple',
+                                    'Twin',
+                                    'Apartment'
+                                ]
+                            };
+                        } else {
+                            data = null;
+                        }
+
+                        var myChart = new Chart(ctx, {
+                            type: 'doughnut',
+                            data: data
+                        });
+
+                    } else if (type === 'resByMonth') {
+                        var reservationsByMonths = [];
+                    }
+
+                    $('#myChart').css('background-color', 'rgba(256, 256, 256, 0.6)');
+                    _self.loading = false;
+                })
+        }
     }
 
 })();
